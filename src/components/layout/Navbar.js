6482e6c05bcbf83a0bb9674f47aa5acb346489bc
Zachary Skiles <skilesz@purdue.edu>
2020-06-26 00:27:24 -0400

    Started Navbar, added most functionality

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Navbar = () => {
+  var hidden = true;
+
+  useEffect(() => {
+    window.addEventListener('scroll', (e) => {
+      if (window.pageYOffset > document.documentElement.clientHeight) {
+        document.querySelector('#navbar').className = '';
+        hidden = false;
+      } else {
+        document.querySelector('#navbar').className = 'hidden';
+        hidden = true;
+      }
+    });
+  }, []);
+
   return (
-    <div id='navbar' className='skeleton'>
-      <h2>Navbar</h2>
+    <div id='navbar' className='hidden'>
+      <div className='container'>
+        <h2>
+          <a href='#'>ZS</a>
+        </h2>
+        <ul>
+          <li>
+            <a href='#about'>ABOUT</a>
+          </li>
+          <li>
+            <a href='#experience'>EXPERIENCE</a>
+          </li>
+          <li>
+            <a href='#projects'>PROJECTS</a>
+          </li>
+          <li>
+            <a href='#skills'>SKILLS</a>
+          </li>
+          <li>
+            <a href='#contact'>CONTACT</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
